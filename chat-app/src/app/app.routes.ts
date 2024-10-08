fe6d0d9177b00f30b4d0d037228793b6a1a21e24
Anthony <91128594+yellowthing0@users.noreply.github.com>
2024-10-09 02:45:22 +1000

    fixed group chat routing

--- chat-app/src/app/app.routes.ts
+++ chat-app/src/app/app.routes.ts
@@ -9,12 +9,14 @@ export const routes: Routes = [
   },
   {
     path: 'groups',
-    loadComponent: () => import('./components/groups/groups.component').then(m => m.GroupsComponent),
-    canActivate: [AuthGuard] // Protect the 'groups' route with the AuthGuard
+    loadComponent: () => import('./components/groups/groups.component').then(m => m.GroupsComponent)
+  },
+  {
+    path: 'groups/:groupId/channels',
+    loadComponent: () => import('./components/channels/channels.component').then(m => m.ChannelsComponent)
   },
   {
     path: 'profile',
-    loadComponent: () => import('./components/profile/profile.component').then(m => m.ProfileComponent),
-    canActivate: [AuthGuard] // Protect the 'profile' route with the AuthGuard
+    loadComponent: () => import('./components/profile/profile.component').then(m => m.ProfileComponent)
   }
 ];
